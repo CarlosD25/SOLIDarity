1a061357964d09323062a6a76e92b20203d2ee94
CarlosD25 <sanchezpalominocarlosdaniel@gmail.com>
2025-11-10 15:13:21 -0500

    Se agregó la carpeta de excepciones en ms-donación

---
+++ ms-architecture/donacion-service/src/main/java/exception/DonationFailedException.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exception;
+
+/**
+ *
+ * @author Carlo
+ */
+public class DonationFailedException extends RuntimeException{
+
+    public DonationFailedException(String message) {
+        super(message);
+    }
+    
+}
