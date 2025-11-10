c22293708ca46ac989d0540056a41f42f99caffa
CarlosD25 <sanchezpalominocarlosdaniel@gmail.com>
2025-11-10 15:01:32 -0500

    Últimos cambios en la capa de presentación en ms-usuarios

--- ms-architecture/user-service/src/main/java/presentation/UserController.java
+++ ms-architecture/user-service/src/main/java/presentation/UserController.java
@@ -6,9 +6,16 @@ package presentation;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dto.BeneficiarioStatusResponseDTO;
+import dto.PdfDTO;
+import dto.PdfEstadoRequestDTO;
+import dto.UserLastPdfDTO;
 import dto.UserRequestDTO;
 import dto.UserResponseDTO;
+import exception.NotificationServiceException;
+import exception.PdfNotFoundException;
+import exception.UserNotFoundException;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -19,7 +26,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
-import javax.swing.JOptionPane;
 import service.UserService;
 import service.UserServiceImpl;
 
@@ -31,29 +37,34 @@ import service.UserServiceImpl;
 @MultipartConfig
 public class UserController extends HttpServlet {
 
-    private final UserService userService = new UserServiceImpl();
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final UserService userService;
+    private final ObjectMapper objectMapper;
+
+    public UserController() {
+        userService = new UserServiceImpl();
+        objectMapper = new ObjectMapper();
+    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String pathInfo = req.getPathInfo();
-
-        if (pathInfo != null && pathInfo.endsWith("/pdfs")) {
-            String[] parts = pathInfo.split("/");
-            int userId = Integer.parseInt(parts[1]); 
-
-            Part filePart = req.getPart("file");
-            String filename = filePart.getSubmittedFileName();
-            InputStream fileContent = filePart.getInputStream();
-
-            userService.saveUserPDF(userId, filename, fileContent);
-
-            resp.setStatus(HttpServletResponse.SC_CREATED);
-            resp.getWriter().write("PDF subido correctamente.");
-            return;
-        }
-
         try {
+
+            String pathInfo = req.getPathInfo();
+
+            if (pathInfo != null && pathInfo.endsWith("/pdf")) {
+                String[] parts = pathInfo.split("/");
+                int userId = Integer.parseInt(parts[1]);
+
+                Part filePart = req.getPart("file");
+                String filename = filePart.getSubmittedFileName();
+                InputStream fileContent = filePart.getInputStream();
+
+                userService.saveUserPDF(userId, filename, fileContent);
+
+                resp.setStatus(HttpServletResponse.SC_CREATED);
+                resp.getWriter().write("PDF subido correctamente.");
+                return;
+            }
 
             BufferedReader br = req.getReader();
             UserRequestDTO userRequestDTO = objectMapper.readValue(br, UserRequestDTO.class);
@@ -64,6 +75,9 @@ public class UserController extends HttpServlet {
             resp.setCharacterEncoding("UTF-8");
             resp.setStatus(HttpServletResponse.SC_CREATED);
             objectMapper.writeValue(resp.getWriter(), userResponseDTO);
+        } catch (UserNotFoundException ex) {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            resp.getWriter().write(ex.getMessage());
         } catch (Exception ex) {
             resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
@@ -75,6 +89,23 @@ public class UserController extends HttpServlet {
 
         try {
             String pathInfo = req.getPathInfo();
+
+            if (pathInfo != null && pathInfo.equals("/pdf")) {
+                String estado = req.getParameter("estado");
+                if (estado == null || estado.isEmpty()) {
+                    resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                    resp.getWriter().write("Debe proporcionar un estado válido.");
+                    return;
+                }
+
+                List<UserLastPdfDTO> usuarios = userService.findUsersWithLastPdfByState(estado);
+
+                resp.setStatus(HttpServletResponse.SC_OK);
+                resp.setContentType("application/json");
+                resp.setCharacterEncoding("UTF-8");
+                objectMapper.writeValue(resp.getWriter(), usuarios);
+                return;
+            }
 
             if (pathInfo == null || pathInfo.equals("/")) {
                 List<UserResponseDTO> usuarios = userService.getAll();
@@ -104,7 +135,7 @@ public class UserController extends HttpServlet {
 
             if (pathInfo.matches("/\\d+/beneficiario")) {
                 String[] parts = pathInfo.split("/");
-                int userId = Integer.parseInt(parts[1]); 
+                int userId = Integer.parseInt(parts[1]);
 
                 BeneficiarioStatusResponseDTO statusResponse = userService.getBeneficiarioStatus(userId);
 
@@ -120,9 +151,43 @@ public class UserController extends HttpServlet {
                 return;
             }
 
+            if (pathInfo != null && pathInfo.matches("/\\d+/pdf")) {
+                String[] parts = pathInfo.split("/");
+                int userId = Integer.parseInt(parts[1]);
+
+                PdfDTO pdfDto = userService.findLatestPdfByUserId(userId);
+
+                resp.setContentType("application/pdf");
+                resp.setHeader("Content-Disposition", "inline; filename=\"" + pdfDto.getFileName() + "\"");
+
+                try (InputStream is = pdfDto.getPdfStream(); ServletOutputStream os = resp.getOutputStream()) {
+
+                    byte[] buffer = new byte[8192];
+                    int bytesRead;
+                    while ((bytesRead = is.read(buffer)) != -1) {
+                        os.write(buffer, 0, bytesRead);
+                    }
+                    os.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+
+                }
+
+                return;
+            }
+
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+
         } catch (NumberFormatException ex) {
             resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             resp.getWriter().write("El ID debe ser numérico.");
+        } catch (PdfNotFoundException ex) {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            resp.getWriter().write(ex.getMessage());
+        } catch (UserNotFoundException ex) {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            resp.getWriter().write(ex.getMessage());
         } catch (Exception ex) {
             ex.printStackTrace();
             resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
@@ -154,17 +219,44 @@ public class UserController extends HttpServlet {
                 userService.activate(id);
             } else if (pathInfo.endsWith("/deactivate")) {
                 userService.deactivate(id);
+            } else if (pathInfo.endsWith("/pdf/status")) {
+
+                PdfEstadoRequestDTO pdfEstadoRequestDTO = new ObjectMapper()
+                        .readValue(req.getReader(), PdfEstadoRequestDTO.class);
+
+                userService.actualizarPdfEstado(id, pdfEstadoRequestDTO);
+
+                resp.setStatus(HttpServletResponse.SC_OK);
+                return;
+            } else if (parts.length == 2) {
+
+                UserRequestDTO userRequestDTO = new ObjectMapper()
+                        .readValue(req.getReader(), UserRequestDTO.class);
+
+                UserResponseDTO updatedUser = userService.update(id, userRequestDTO);
+
+                resp.setContentType("application/json");
+                resp.setStatus(HttpServletResponse.SC_OK);
+                resp.getWriter().write(new ObjectMapper().writeValueAsString(updatedUser));
+                return;
+
             } else {
-                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-                resp.getWriter().write("Acción no reconocida. Use /activate o /deactivate");
+                resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
                 return;
             }
-
-            resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
 
         } catch (NumberFormatException ex) {
             resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             resp.getWriter().write("El ID debe ser numérico.");
+        } catch (UserNotFoundException ex) {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            resp.getWriter().write(ex.getMessage());
+        } catch (PdfNotFoundException ex) {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            resp.getWriter().write(ex.getMessage());
+        } catch (NotificationServiceException ex) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            resp.getWriter().write(ex.getMessage());
         } catch (Exception ex) {
             ex.printStackTrace();
             resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
