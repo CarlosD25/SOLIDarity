ec4e8e88e7459e0bd9093556625dd46a41cee1da
CarlosD25 <sanchezpalominocarlosdaniel@gmail.com>
2025-11-10 15:12:13 -0500

    últimos cambios en la capa de servicio en ms-donación

--- ms-architecture/donacion-service/src/main/java/service/DonacionServiceImpl.java
+++ ms-architecture/donacion-service/src/main/java/service/DonacionServiceImpl.java
@@ -4,8 +4,13 @@
  */
 package service;
 
+import client.CampañaClient;
 import dto.DonacionRequestDTO;
 import dto.DonacionResponseDTO;
+import exception.DonationFailedException;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.http.HttpResponse;
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.List;
@@ -19,25 +24,45 @@ import persistencia.DonacionDaoImpl;
  *
  * @author Carlo
  */
-public class DonacionServiceImpl implements DonacionService{
-    
-    private final DonacionDao donacionDao = new DonacionDaoImpl();
-    private final DonacionMapper donacionMapper = new DonacionMapperImpl();
+public class DonacionServiceImpl implements DonacionService {
 
+    private final DonacionDao donacionDao;
+    private final DonacionMapper donacionMapper;
+    private final CampañaClient client;
+
+    public DonacionServiceImpl(){
+        donacionDao = new DonacionDaoImpl();
+        donacionMapper = DonacionMapperImpl.getInstace();
+        client = CampañaClient.getInstance();
+    }
+    
     @Override
     public DonacionResponseDTO save(DonacionRequestDTO requestDTO) {
-        
-        Donacion d = donacionMapper.toEntity(requestDTO);
-        d.setFecha(Timestamp.from(Instant.now()));
-        return donacionMapper.toDTO(donacionDao.save(d));
-        
+
+        try {
+            HttpResponse<String> response = client.actualizarMontoConDonacion(requestDTO);
+
+            if (response.statusCode() != HttpURLConnection.HTTP_NO_CONTENT) {
+                throw new DonationFailedException("No se pudo procesar la donación: Código " + response.statusCode());
+            }
+
+            Donacion d = donacionMapper.toEntity(requestDTO);
+            d.setFecha(Timestamp.from(Instant.now()));
+
+            Donacion saved = donacionDao.save(d);
+            return donacionMapper.toDTO(saved);
+
+        } catch (IOException | InterruptedException ex) {
+            throw new DonationFailedException("Error de comunicación con el servicio de campañas");
+        }
+
     }
 
     @Override
     public List<DonacionResponseDTO> getByCampañaId(int id) {
-        
+
         return donacionDao.getByCampañaId(id).stream().map(donacionMapper::toDTO).toList();
-        
+
     }
-    
+
 }
