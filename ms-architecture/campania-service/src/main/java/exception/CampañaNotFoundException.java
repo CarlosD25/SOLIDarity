ecb799532f475aa638f21a03b40b56a881211a61
CarlosD25 <sanchezpalominocarlosdaniel@gmail.com>
2025-11-10 15:19:42 -0500

    Se agregó la carpeta de excepciones en ms-campaña

---
+++ ms-architecture/campania-service/src/main/java/exception/CampañaNotFoundException.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exception;
+
+/**
+ *
+ * @author Carlo
+ */
+public class CampañaNotFoundException extends RuntimeException{
+
+    public CampañaNotFoundException(String message) {
+        super(message);
+    }
+    
+}
