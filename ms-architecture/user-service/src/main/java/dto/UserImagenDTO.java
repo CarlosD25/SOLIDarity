2b0a8fe3a5fcfe9aaa4613f8c8cfecd14bf8b86b
CarlosD25 <sanchezpalominocarlosdaniel@gmail.com>
2025-11-12 22:11:17 -0500

    Se agregó el dto para la imagen del usuario en ms-usuarios

---
+++ ms-architecture/user-service/src/main/java/dto/UserImagenDTO.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package dto;
+
+/**
+ *
+ * @author Carlo
+ */
+public class UserImagenDTO {
+
+    private String imagenUrl;
+
+    public UserImagenDTO() {
+    }
+
+    public UserImagenDTO(String imagenUrl) {
+        this.imagenUrl = imagenUrl;
+    }
+
+    public String getImagenUrl() {
+        return imagenUrl;
+    }
+
+    public void setImagenUrl(String imagenUrl) {
+        this.imagenUrl = imagenUrl;
+    }
+    
+}
